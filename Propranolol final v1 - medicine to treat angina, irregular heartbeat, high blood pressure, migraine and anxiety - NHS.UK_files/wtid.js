9df55d7a12582ad23ee7028c0f68427d312825c7
bencullimore <ben.cullimore@nhs.net>
2018-01-31 16:12:04 +0000

    hackmatack

---
+++ Propranolol final v1 - medicine to treat angina, irregular heartbeat, high blood pressure, migraine and anxiety - NHS.UK_files/wtid.js
@@ -0,0 +1,3 @@
+if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
+Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"","gTempWtId":"1f71a990-5d5f-428a-aaeb-b93a59ab5506","gWtAccountRollup":""});
+}
